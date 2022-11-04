6abf675464a6cbf8c4cee769ed506146cbddcd32
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-11-04 10:21:04 -0400

    ch8: message defs
    https://github.com/Callum-A/cosmwasm-zero-to-hero/tree/main/08%20-%20ExecuteMsg
    
    defining all messages in a separate file seems at first glance like a
    lot of ceremony to NEAR's approach of using JSON-serialized inputs.

--- src/msg.rs
+++ src/msg.rs
@@ -10,7 +10,21 @@ pub struct InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    CustomMsg { val: String },
+    CreatePoll {
+        poll_id: String,
+        question: String,
+        options: Vec<String>,
+    },
+    Vote {
+        poll_id: String,
+        vote: String,
+    },
+    DeletePoll {
+        poll_id: String,
+    },
+    RevokeVote {
+        poll_id: String,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
